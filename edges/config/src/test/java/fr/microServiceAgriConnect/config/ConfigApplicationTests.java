5cdfcfa74ba6a935451f0b801cfec4a8822168c6
gwenael <gwenael0719@gmail.com>
2024-06-06 22:09:45 +0200

    initial commit

---
+++ edges/config/src/test/java/fr/microServiceAgriConnect/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.microServiceAgriConnect.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ edges/service-registry/src/test/java/fr/microServiceAgriConnect/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.microServiceAgriConnect.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
