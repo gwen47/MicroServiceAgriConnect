4ca4563374b1ce2674bd2654b40e0a6675ef20de
Shan <theo.pessegue2@gmail.com>
2024-06-09 00:16:12 +0200

    add new routes

--- business/sensors/src/main/java/fr/microservice/sensor/controllers/SensorActuatorController.java
+++ business/sensors/src/main/java/fr/microservice/sensor/controllers/SensorActuatorController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import fr.microservice.sensor.entities.Actuator;
+import fr.microservice.sensor.entities.IrrigationLog;
 import fr.microservice.sensor.entities.Sensor;
 import fr.microservice.sensor.services.SensorActuatorService;
 
@@ -31,6 +32,11 @@ public class SensorActuatorController {
         return sensorActuatorService.saveSensor(sensor);
     }
 
+    @DeleteMapping("/sensor/{id}")
+    public void deleteSensor(@PathVariable Long id) {
+        sensorActuatorService.deleteSensor(id);
+    }
+
     @GetMapping("/actuator/{id}")
     public Actuator getActuator(@PathVariable Long id) {
         return sensorActuatorService.getActuator(id);
@@ -39,5 +45,35 @@ public class SensorActuatorController {
     @PostMapping("/actuator")
     public Actuator createActuator(@RequestBody Actuator actuator) {
         return sensorActuatorService.saveActuator(actuator);
+    }
+
+    @DeleteMapping("/actuator/{id}")
+    public void deleteActuator(@PathVariable Long id) {
+        sensorActuatorService.deleteActuator(id);
+    }
+
+    @PostMapping("/actuator/start-irrigation/{id}")
+    public void startIrrigation(@PathVariable Long id, @RequestParam long duration) {
+        sensorActuatorService.startIrrigation(id, duration);
+    }
+
+    @PostMapping("/actuator/stop-irrigation/{id}")
+    public void stopIrrigation(@PathVariable Long id) {
+        sensorActuatorService.stopIrrigation(id);
+    }
+
+    @GetMapping("/irrigation-logs")
+    public List<IrrigationLog> getIrrigationLogs() {
+        return sensorActuatorService.getIrrigationLogs();
+    }
+
+    @PostMapping("/sensor/update-interval/{id}")
+    public void updateMeasurementInterval(@PathVariable Long id, @RequestParam int interval) {
+        sensorActuatorService.updateMeasurementInterval(id, interval);
+    }
+
+    @PostMapping("/sensors/update-interval")
+    public void updateMeasurementIntervalForAll(@RequestParam int interval) {
+        sensorActuatorService.updateMeasurementIntervalForAll(interval);
     }
 }
--- business/sensors/src/main/java/fr/microservice/sensor/services/SensorActuatorService.java
+++ business/sensors/src/main/java/fr/microservice/sensor/services/SensorActuatorService.java
@@ -1,13 +1,16 @@
 package fr.microservice.sensor.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.microservice.sensor.entities.Actuator;
+import fr.microservice.sensor.entities.IrrigationLog;
 import fr.microservice.sensor.entities.Sensor;
 import fr.microservice.sensor.repositories.ActuatorRepository;
+import fr.microservice.sensor.repositories.IrrigationLogRepository;
 import fr.microservice.sensor.repositories.SensorRepository;
 
 @Service
@@ -18,6 +21,9 @@ public class SensorActuatorService {
 
     @Autowired
     private ActuatorRepository actuatorRepository;
+
+    @Autowired
+    private IrrigationLogRepository irrigationLogRepository;
 
     public List<Sensor> listAllSensors() {
         return sensorRepository.findAll();
@@ -38,5 +44,56 @@ public class SensorActuatorService {
     public Actuator saveActuator(Actuator actuator) {
         return actuatorRepository.save(actuator);
     }
-}
 
+    public void deleteSensor(Long id) {
+        sensorRepository.deleteById(id);
+    }
+
+    public void deleteActuator(Long id) {
+        actuatorRepository.deleteById(id);
+    }
+
+    public void startIrrigation(Long actuatorId, long duration) {
+        Actuator actuator = actuatorRepository.findById(actuatorId).orElseThrow(() -> new RuntimeException("Actuator not found"));
+        actuator.setIrrigating(true);
+        actuator.setIrrigationDuration(duration);
+        actuatorRepository.save(actuator);
+
+        IrrigationLog log = new IrrigationLog();
+        log.setActuator(actuator);
+        log.setStartTime(LocalDateTime.now());
+        log.setStatus("started");
+        irrigationLogRepository.save(log);
+    }
+
+    public void stopIrrigation(Long actuatorId) {
+        Actuator actuator = actuatorRepository.findById(actuatorId).orElseThrow(() -> new RuntimeException("Actuator not found"));
+        actuator.setIrrigating(false);
+        actuator.setIrrigationDuration(0);
+        actuatorRepository.save(actuator);
+
+        IrrigationLog log = new IrrigationLog();
+        log.setActuator(actuator);
+        log.setEndTime(LocalDateTime.now());
+        log.setStatus("ended");
+        irrigationLogRepository.save(log);
+    }
+
+    public List<IrrigationLog> getIrrigationLogs() {
+        return irrigationLogRepository.findAll();
+    }
+
+    public void updateMeasurementInterval(Long sensorId, int interval) {
+        Sensor sensor = sensorRepository.findById(sensorId).orElseThrow(() -> new RuntimeException("Sensor not found"));
+        sensor.setMeasurementInterval(interval);
+        sensorRepository.save(sensor);
+    }
+
+    public void updateMeasurementIntervalForAll(int interval) {
+        List<Sensor> sensors = sensorRepository.findAll();
+        for (Sensor sensor : sensors) {
+            sensor.setMeasurementInterval(interval);
+        }
+        sensorRepository.saveAll(sensors);
+    }
+}
